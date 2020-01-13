1cb536472801b4050896023fc34cf99038bfe103
Arthur Barbosa <arthurbruno03@gmail.com>
2020-01-13 17:53:01 -0400

    filtrando ofertas em destaque

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -12,7 +12,7 @@ export class OfertasService {
 
 
   public getOfertas(): Promise<Oferta[]> {
-   return this.http.get('http://localhost:3000/ofertas')
+   return this.http.get('http://localhost:3000/ofertas?destaque=true')
       .toPromise()
       .then((resposta: any) => resposta);
   }
